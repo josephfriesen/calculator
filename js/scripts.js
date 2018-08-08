07953d4a72ca90229d280eea10fc82127f9b513b
Joseph Friesen <friesen.josephc@gmail.com>
2018-08-08 10:57:54 -0700

    Add multiplication and division functions.

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,8 @@ $(document).ready(function() {
     var stringNum2 = $("#add2").val();
     var result = add(parseInt(stringNum1), parseInt(stringNum2));
     var stringResult = result.toString();
-    var outputSentence = stringNum1 + " + " + stringNum2 + " = " + stringResult;
-    $("#output").text(outputSentence);
+    var outputSentence = stringNum1 + " + " + stringNum2 + " = " + stringResult + "<br>";
+    $("#output").prepend(outputSentence);
   });
 
   $("form#sub").submit(function(event) {
@@ -35,7 +35,32 @@ $(document).ready(function() {
     var stringNum2 = $("#sub2").val();
     var result = subtract(parseInt(stringNum1), parseInt(stringNum2));
     var stringResult = result.toString();
-    var outputSentence = stringNum1 + " - " + stringNum2 + " = " + stringResult;
-    $("#output").text(outputSentence);
+    var outputSentence = stringNum1 + " - " + stringNum2 + " = " + stringResult + "<br>";
+    $("#output").prepend(outputSentence);
+  });
+
+  $("form#multi").submit(function(event) {
+    event.preventDefault();
+    var stringNum1 = $("#multi1").val();
+    var stringNum2 = $("#multi2").val();
+    var result = multiply(parseInt(stringNum1), parseInt(stringNum2));
+    var stringResult = result.toString();
+    var outputSentence = stringNum1 + " x " + stringNum2 + " = " + stringResult + "<br>";
+    $("#output").prepend(outputSentence);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var stringNum1 = $("#divide1").val();
+    var stringNum2 = $("#divide2").val();
+    if (stringNum2 == "0") {
+      $("#output").prepend("I told you the denominator can't be zero!<br>");
+    }
+    else {
+      var result = divide(parseInt(stringNum1), parseInt(stringNum2));
+      var stringResult = result.toFixed(3).toString();
+      var outputSentence = stringNum1 + " / " + stringNum2 + " = " + stringResult + "<br>";
+      $("#output").prepend(outputSentence);
+    };
   });
 });
